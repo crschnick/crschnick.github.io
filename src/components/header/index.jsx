11602febc9b7a0d843b25c1cbd1be641bd03b671
Christopher Schnick <crschnick@xpipe.io>
2023-06-13 02:18:01 +0200

    Update data

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import get from 'lodash/get';
 import React from 'react';
 
-import profileImg from '../../images/profile.png';
+import profileImg from '../../images/profile.jpg';
 import resume from '../../../static/resume.pdf';
 
 const classes = {
